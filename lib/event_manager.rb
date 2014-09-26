aebf34088bac06d4d162c5458bbd2795ca1a61ec
David Bayendor <dbayendor@gmail.com>
2014-09-26 15:56:38 -0600

    Get legislator names
    * Parse out of sunlight-congress gem
    
    * Include in output

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,7 @@
 require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = 'e179a6973728c4dd3fb1204283aaccb5'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
@@ -9,9 +12,20 @@ puts "EventManager Initialized"
 if File.exist? '../event_attendees.csv'
   contents = CSV.open '../event_attendees.csv', headers: true,
     header_converters: :symbol
-  contents.each do |row|
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    puts "#{name} #{zipcode}"
+end
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
   end
+
+  legislators_string = legislator_names.join(', ')
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
