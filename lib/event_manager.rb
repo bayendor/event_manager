0046778a603daab739490f5900034995f4ccb7d5
David Bayendor <dbayendor@gmail.com>
2014-09-27 07:49:24 -0600

    Complete Iteration 4
    * Add erb file for form_letter
    
    * Create #save_letters method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = 'e179a6973728c4dd3fb1204283aaccb5'
 
@@ -9,12 +10,16 @@ end
 
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
 
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir('output') unless Dir.exists? 'output'
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
   end
-
-  legislator_names.join(', ')
 end
 
 puts 'EventManager Initialized'
@@ -27,21 +32,25 @@ else
 end
 
 if File.exist? 'form_letter.html'
-  template_letter = File.read "form_letter.html"
+  template_letter = File.read "form_letter.erb"
+  erb_template = ERB.new template_letter
 else
   puts 'File not found'
 end
 
 print 'Working'
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME',name)
-  personal_letter.gsub!('LEGISLATORS',legislators)
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letters(id, form_letter)
+
   print '.'
 end
 
